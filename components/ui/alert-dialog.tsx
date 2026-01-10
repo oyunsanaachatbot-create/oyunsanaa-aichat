424ac49d9879ab10985604a016f12b62f02c6540
oyunsanaachatbot-create <oyunsanaachatbot@gmail.com>
2026-01-10 17:10:59 +0800

    Update alert-dialog.tsx

--- components/ui/alert-dialog.tsx
+++ components/ui/alert-dialog.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { AlertDialog as AlertDialogPrimitive } from "radix-ui";
+import * as AlertDialogPrimitive from "@radix-ui/react-alert-dialog";
 import * as React from "react";
 import { buttonVariants } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
