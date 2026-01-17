3032474224758f0c19c59ec522f8aa4388f962d1
oyunsanaachatbot-create <oyunsanaachatbot@gmail.com>
2026-01-18 00:16:27 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,13 +4,22 @@ const nextConfig: NextConfig = {
   cacheComponents: true,
   images: {
     remotePatterns: [
+      // ✅ template default
       {
         hostname: "avatar.vercel.sh",
       },
+
+      // ✅ Vercel Blob
       {
         protocol: "https",
-        //https://nextjs.org/docs/messages/next-image-unconfigured-host
+        // https://nextjs.org/docs/messages/next-image-unconfigured-host
         hostname: "*.public.blob.vercel-storage.com",
+      },
+
+      // ✅ Unsplash (Ebook cards images)
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
       },
     ],
   },
