f72474b787e006c716a49be52176e076064e26d0
oyunsanaachatbot-create <oyunsanaachatbot@gmail.com>
2026-01-21 10:56:58 +0800

    Update greeting.tsx

--- components/greeting.tsx
+++ components/greeting.tsx
@@ -13,7 +13,7 @@ export const Greeting = () => {
         initial={{ opacity: 0, y: 10 }}
         transition={{ delay: 0.5 }}
       >
-        Hello there!
+        Сайн уу, Оюунсанаа байна 😊. 
       </motion.div>
       <motion.div
         animate={{ opacity: 1, y: 0 }}
@@ -22,7 +22,7 @@ export const Greeting = () => {
         initial={{ opacity: 0, y: 10 }}
         transition={{ delay: 0.6 }}
       >
-        How can I help you today?
+        Таньд юугаар туслах вэ?
       </motion.div>
     </div>
   );
