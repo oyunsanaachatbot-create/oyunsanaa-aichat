549cea03c8ad8ad41e7e0f6d0b77eae71e8b6b64
oyunsanaachatbot-create <oyunsanaachatbot@gmail.com>
2026-01-29 10:53:19 +0800

    Update page.tsx

--- app/(chat)/mind/relations/tests/page.tsx
+++ app/(chat)/mind/relations/tests/page.tsx
@@ -1,16 +1,21 @@
 import Link from "next/link";
-import { TESTS } from "@/lib/apps/relations/tests/testsRegistry";
+import { TESTS } from "@/lib/apps/relations/tests/definitions";
 
 export default function RelationsTestsPage() {
   return (
     <div>
       <h1>Харилцааны тестүүд</h1>
 
-      {TESTS.map(t => (
-        <Link key={t.slug} href={`/mind/relations/tests/${t.slug}`}>
-          {t.title}
-        </Link>
+      {TESTS.map((t) => (
+        <div key={t.slug}>
+          <Link href={`/mind/relations/tests/${t.slug}`}>{t.title}</Link>
+        </div>
       ))}
+
+      {/* ✅ /chat чинь 404 байгаа болохоор түр / рүү */}
+      <div style={{ marginTop: 16 }}>
+        <Link href="/">Чат руу</Link>
+      </div>
     </div>
   );
 }
