6dd3f3a7a0d96e126c09d06ec04b457c3986c86a
oyunsanaachatbot-create <oyunsanaachatbot@gmail.com>
2026-01-30 21:59:53 +0800

    Update page.tsx

--- app/(chat)/mind/relations/tests/[slug]/page.tsx
+++ app/(chat)/mind/relations/tests/[slug]/page.tsx
@@ -2,7 +2,7 @@
 
 import { useMemo } from "react";
 import { useParams, useRouter } from "next/navigation";
-import styles from "../tests.module.css";
+import styles from "../tests.module.css"
 
 import TopBar from "../_components/TopBar";
 import TestRunner from "@/components/apps/relations/tests/TestRunner";
