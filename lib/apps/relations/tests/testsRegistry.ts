6d75c94b921b472faac5b695ded731ff1ac05ca8
oyunsanaachatbot-create <oyunsanaachatbot@gmail.com>
2026-01-30 10:45:44 +0800

    Update testsRegistry.ts

--- lib/apps/relations/tests/testsRegistry.ts
+++ lib/apps/relations/tests/testsRegistry.ts
@@ -1,16 +1,27 @@
 import type { TestDefinition } from "./types";
+
 import { personalityBasic } from "./definitions/personalityBasic";
 import { communicationStyle } from "./definitions/communicationStyle";
 
+import { listening } from "./definitions/listening";
+import { empathy } from "./definitions/empathy";
+import { boundaries } from "./definitions/boundaries";
+import { conflict } from "./definitions/conflict";
+import { trust } from "./definitions/trust";
+import { toxicBehavior } from "./definitions/toxicBehavior";
+
 export const TESTS: TestDefinition[] = [
+  // эхний 2
   personalityBasic,
   communicationStyle,
+
+  // шинэ 5
+  listening,
+  empathy,
+  boundaries,
+  conflict,
+  trust,
+
+  // токсик
+  toxicBehavior,
 ];
-
-export function getTestById(id: string) {
-  return TESTS.find((t) => t.id === id);
-}
-
-export function getTestBySlug(slug: string) {
-  return TESTS.find((t) => t.slug === slug);
-}
